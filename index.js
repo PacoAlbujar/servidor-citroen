a5e1e9607c91fb5538b2ef43c629b024f79780b8
PacoAlbujar <153760051+PacoAlbujar@users.noreply.github.com>
2026-08-19 11:12:47 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const WebSocket = require('ws');
+const port = process.env.PORT || 8080;
+const wss = new WebSocket.Server({ port });
+
+console.log(`Servidor multijugador activo en el puerto ${port}`);
+
+wss.on('connection', (ws) => {
+  console.log('¡Un Citroën se ha conectado!');
+
+  ws.on('message', (message) => {
+    // Reenviar la posición a todos los demás jugadores conectados
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message.toString());
+      }
+    });
+  });
+
+  ws.on('close', () => {
+    console.log('Un Citroën se ha desconectado.');
+  });
+});
